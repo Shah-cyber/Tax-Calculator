277def56d4914d892867c119d671c84a46bb0741
SHAHIZZUL IKHWAN BIN SAZALI <shahizzulikhwan@gmail.com>
2025-02-26 16:35:39 +0800

    error vercel

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,8 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    build: {
+        outDir: 'public/build', // Ensures Vercel finds assets
+        emptyOutDir: true,
+    }
 });
